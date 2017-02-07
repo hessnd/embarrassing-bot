30a9d11ae07eef43c0bc2b403dc36e623a654c0e
hessnd <n.hess@bigspaceship.com>
2017-02-07 15:32:09 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const server = app.listen(3000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+app.use(express.static('public'));
+
+app.post('/', (req, res) => {
+  let text = req.body.text;
+  console.log(text);
+  
+  if (text == 'frosh') {
+    let data = {
+      response_type: 'in_channel', // public to channel
+      text: 'look at ' + text,
+      attachments: [
+        {
+          'image_url': 'http://9c0b4c7c.ngrok.io/images/frosh.JPEG',
+        }
+    ]};
+    res.json(data);
+  }
+});
