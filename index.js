a0c1848d72a6f839dc65279aeca6501be2317ce5
hessnd <n.hess@bigspaceship.com>
2017-02-08 09:59:30 -0500

    added dotenv for environment variables

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.load();
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
